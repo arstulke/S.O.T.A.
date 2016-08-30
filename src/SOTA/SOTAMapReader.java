4a16511f96215a749a1e9ead9806bf1dcdb22e09
KarlAbert <karl.abert@btc-ag.com>
2016-08-30 12:54:24 +0200

    MapReader

--- src/SOTA/SOTAMapReader.java
+++ src/SOTA/SOTAMapReader.java
@@ -5,21 +5,35 @@ import java.io.*;
 
 public class SOTAMapReader {
 
-    char[][] map;
+    static String[][] map;
+    static int mapLength = 100;
 
-    void readMap(String path){
+    static String[][][] eventMap;
+    static char player = 'X';
+
+    public static void readMap(String path) {
         File file = new File(path);
-        try (BufferedReader br = new BufferedReader(new FileReader(file))) {
+        try  {
+            BufferedReader br = new BufferedReader(new FileReader(file));
             String line;
             int i = 0;
             try {
                 while ((line = br.readLine()) != null) {
-                    if(!line.startsWith("&c")) {
-                        for (int k = 0; k < line.toCharArray().length; k++) {
-                            map[i][k] = line.toCharArray()[k];
+                    if (line.startsWith(":")) {
+                        for (int k = 1; k < mapLength; k++) {
+                            try {
+                                map[i][k] = Character.toString(line.charAt(k));
+                            }catch(Exception ignore){
+                                map[i][k] = " ";
+                            }
                         }
                         i++;
+                    } else if (line.startsWith("player:")) {
+                        player = line.split(":")[1].toCharArray()[0];
+                    } else if (line.startsWith("-")) {
+                       mapLength = line.length();
                     }
+
                 }
             } catch (IOException e) {
                 e.printStackTrace();
@@ -27,5 +41,13 @@ public class SOTAMapReader {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    public char getPlayer() {
+        return player;
+    }
+
+    public static String[][][] getMap() {
+        return eventMap;
     }
 }
